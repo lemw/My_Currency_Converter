aa3701f10edfe01aa5b04e2ed0abc1949c5ef759
Lars Wattsgard <lars@wattsgard.se>
2015-10-15 20:45:45 +0200

    Initial commit

---
+++ app/src/main/java/se/wattsgard/currency_converter/mycurrencyconverter/Currency_Converter.java
@@ -0,0 +1,13 @@
+package se.wattsgard.currency_converter.mycurrencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Currency_Converter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency_converter);
+    }
+}
